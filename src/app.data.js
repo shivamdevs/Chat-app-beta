8f2ca844e701d30ccfe79ac6ea5124489611a040
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2023-01-20 12:12:12 +0530

    Code update 3.b.a
    Added versioning.

--- src/app.data.js
+++ src/app.data.js
@@ -1,6 +1,6 @@
 const app = {
     name: 'MeChat',
-    version: '1.0.0',
+    version: '0.3.b.a',
     copyright: <a href="//myoasis.tech" target="_blank" rel="noreferrer">© My Oasis Tech</a>,
     updated: 'January 17, 2023 02:00 AM',
     bucket: "chat:",
--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -23,9 +23,8 @@ function Default() {
                 <lottie-player src="https://assets.myoasis.tech/chat/green_blue_char_02020.json" background="transparent" speed=".8" style={{ maxWidth: "400px", width: "100%", margin: "auto" }} loop autoplay></lottie-player>
                 <div className={css.mbFooter}>
                     <button className={classNames('opbutton blue', css.mbButton)} onClick={() => navigate('/accounts')}>Continue to {app.name}</button>
-                    <div className={css.mbBottom}>
-                        By continuing to this website you agree to our <a href={app.pathname + "/legal"} target="_blank" rel="noopener noreferrer">Privacy policy</a> and <a href={app.pathname + "/legal/terms"} target="_blank" rel="noopener noreferrer">Terms of Usage</a>.
-                    </div>
+                    <div className={css.mbBottom}>By continuing to this website you agree to our <a href={app.pathname + "/legal"} target="_blank" rel="noopener noreferrer">Privacy policy</a> and <a href={app.pathname + "/legal/terms"} target="_blank" rel="noopener noreferrer">Terms of Usage</a>.</div>
+                    <div className={css.mbVersion}>Version: {app.version}</div>
                 </div>
             </MobileView>}
             {user && <Routes>
